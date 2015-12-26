3f196d697123bff2f3927e1f49e981f94b2cc414
FedeG <federicog@exo.com.ar>
2015-12-26 00:09:33 -0300

    #2 - Estructura principal de aplicacion django 1.9

---
+++ eventoL/eventol/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for eventoL project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventol.settings")
+application = get_wsgi_application()
